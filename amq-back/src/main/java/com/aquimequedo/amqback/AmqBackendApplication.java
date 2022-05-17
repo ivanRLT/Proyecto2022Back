2c5f2fa41a3a9b14275d5c88b95ad398a1661796
ivancel2014 <ivancel2014@gmail.com>
2022-05-16 23:30:16 -0300

    Inicial

---
+++ amq-back/src/main/java/com/aquimequedo/amqback/AmqBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aquimequedo.amqback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmqBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmqBackendApplication.class, args);
+	}
+
+}
---
+++ amq-back/src/test/java/com/aquimequedo/amqback/AmqBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquimequedo.amqback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmqBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
