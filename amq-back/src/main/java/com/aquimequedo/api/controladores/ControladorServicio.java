a885fec7f8dc46dccb0f8388511d321f91e174e4
ffreddyy <freddyamr@gmail.com>
2022-05-20 07:30:30 -0300

    Se modifica  Controlador de servicio rest

--- amq-back/src/main/java/com/aquimequedo/api/controladores/ControladorServicio.java
+++ amq-back/src/main/java/com/aquimequedo/api/controladores/ControladorServicio.java
@@ -1,5 +1,17 @@
 package com.aquimequedo.api.controladores;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import amq.datatypes.*;
+
+@RestController
+@RequestMapping()
 public class ControladorServicio {
-	
+	@GetMapping(value= "/alojamientos")
+	public List<DtAlojamiento> getAlojamientos(){
+		return null;
+	}
 }
