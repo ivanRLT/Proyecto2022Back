9dbbee8adcb5885231aa795ef56ada943ce44190
ivanRL <ivancel2014@gmail.com>
2022-06-02 18:00:49 -0300

    Alta de anfitrion con alojamiento y habitacion

--- amq-backend/src/main/java/com/amq/datatypes/DtAlojamiento.java
+++ amq-backend/src/main/java/com/amq/datatypes/DtAlojamiento.java
@@ -11,101 +11,56 @@ public class DtAlojamiento {
 	private String descripcion;
 	private DtDireccion direcion;
 	private String nombre;
-	private List<DtHabitacion> habitaciones;
-	/**
-	 * @param activo
-	 * @param descripcion
-	 * @param direcion
-	 * @param nombre
-	 * @param habitaciones
-	 */
-	public DtAlojamiento(Integer id,Boolean activo, String descripcion, DtDireccion direcion, String nombre, List<DtHabitacion> habitaciones ) {
+
+	public DtAlojamiento(Integer id,Boolean activo, String descripcion, DtDireccion direcion, String nombre ) {
 		super();
 		this.id = id;
 		this.activo = activo;
 		this.descripcion = descripcion;
 		this.direcion = direcion;
 		this.nombre = nombre;
-		this.habitaciones = habitaciones;
 	}
-	
-	
 	
 	public Integer getId() {
 		return id;
 	}
 
-
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-
-
-	public List<DtHabitacion> getHabitaciones() {
-		return habitaciones;
-	}
-
-
-
-	public void setHabitaciones(List<DtHabitacion> habitaciones) {
-		this.habitaciones = habitaciones;
-	}
-
-
 
 	public Boolean getActivo() {
 		return activo;
 	}
 
-
-
-	/**
-	 * @return the activo
-	 */
 	public Boolean isActivo() {
 		return activo;
 	}
-	/**
-	 * @param activo the activo to set
-	 */
+	
 	public void setActivo(Boolean activo) {
 		this.activo = activo;
 	}
-	/**
-	 * @return the descripcion
-	 */
+
 	public String getDescripcion() {
 		return descripcion;
 	}
-	/**
-	 * @param descripcion the descripcion to set
-	 */
+
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-	/**
-	 * @return the direcion
-	 */
+
 	public DtDireccion getDirecion() {
 		return direcion;
 	}
-	/**
-	 * @param direcion the direcion to set
-	 */
+
 	public void setDirecion(DtDireccion direcion) {
 		this.direcion = direcion;
 	}
-	/**
-	 * @return the nombre
-	 */
+	
 	public String getNombre() {
 		return nombre;
 	}
-	/**
-	 * @param nombre the nombre to set
-	 */
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
---
+++ amq-backend/src/main/java/com/amq/datatypes/DtAltaAnfitrion.java
@@ -0,0 +1,30 @@
+package com.amq.datatypes;
+
+public class DtAltaAnfitrion {
+	private DtAnfitrion anfitrion;
+	private DtAlojamiento alojamiento;
+	private DtHabitacion habitacion;
+	
+	public DtAltaAnfitrion() {}
+	
+	public DtAnfitrion getAnfitrion() {
+		return anfitrion;
+	}
+	public void setAnfitrion(DtAnfitrion anfitrion) {
+		this.anfitrion = anfitrion;
+	}
+	public DtAlojamiento getAlojamiento() {
+		return alojamiento;
+	}
+	public void setAlojamiento(DtAlojamiento alojamiento) {
+		this.alojamiento = alojamiento;
+	}
+	public DtHabitacion getHabitacion() {
+		return habitacion;
+	}
+	public void setHabitacion(DtHabitacion habitacion) {
+		this.habitacion = habitacion;
+	}
+	
+	
+}
