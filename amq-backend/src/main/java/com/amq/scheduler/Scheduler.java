6d1de2b9774aaf0ef5f3cc2a5acc70c43ce9139b
e297775 <e297775@X0405949.net.in.iantel.com.uy>
2022-07-14 11:15:54 -0300

    Se agrega limpieza de tokens de reseteo de contraseña a Scheduler

--- amq-backend/src/main/java/com/amq/scheduler/Scheduler.java
+++ amq-backend/src/main/java/com/amq/scheduler/Scheduler.java
@@ -5,15 +5,25 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.amq.repositories.RepositoryReserva;
+import com.amq.repositories.RepositoryResetPassword;
 
 @Component
 public class Scheduler {
 	@Autowired
 	RepositoryReserva repoR;
 	
-	@Scheduled(cron = "0 * * * * *")
+	@Autowired
+	RepositoryResetPassword repoPass;
+	
+	@Scheduled(cron = "0 1 1 * * *")
 	public void setReservaEjecutada() {
 		repoR.setReservasEjecutadas();
-	    System.out.println("SCHEDULER OK!");
+	    System.out.println("SCHEDULER: se ejecutó proceso de seteo de estado de reservas ejecutadas.");
+	}
+	
+	@Scheduled(cron = "0 * * * * *")
+	public void limpiarPasswordResetTokens() {
+		repoPass.limpiarVencidos();
+		System.out.println("SCHEDULER: se ejecutó proceso de limpieza de tokens de reseteo de contraseña vencidos.");
 	}
 }
