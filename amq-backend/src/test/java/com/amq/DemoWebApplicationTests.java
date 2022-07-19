370632d0dcd91045aa5b31868f224107e8c172a1
valeri <valeri_macarov@hotmail.com>
2022-07-18 22:46:20 -0300

    mas operaciones de testing.

--- amq-backend/src/test/java/com/amq/DemoWebApplicationTests.java
+++ amq-backend/src/test/java/com/amq/DemoWebApplicationTests.java
@@ -4,14 +4,23 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+//import com.amq.controller.ControladorAlojamientoTest;
+import com.amq.controller.ControladorUsuario;
+import com.amq.model.ModelTest;
+
 //import com.amq.icontroller.IUsuarioRepo;
 //import com.amq.model.Usuarioborrar;
 
-@RunWith(SpringRunner.class)
+//@RunWith(SpringRunner.class)
+//@RunWith(Suite.class)
+//@SuiteClasses({ControladorAlojamientoTest.class, ControladorUsuario.class,
+//	ModelTest.class})
 @SpringBootTest
 public class DemoWebApplicationTests {
 
